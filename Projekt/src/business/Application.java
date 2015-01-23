624172b809abb06edd7b6f97b55f1983135bbcca
paddyp <patrikpezelj@Patriks-MacBook-Pro-2.local>
2015-01-23 12:45:09 +0100

    DBConnection and Query
    Init DBConnection and DBQuery to connect to database and get Querys
    from it !! This is nice =)

--- Projekt/src/business/Application.java
+++ Projekt/src/business/Application.java
@@ -1,10 +1,25 @@
 package business;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+			DBConnection dbcon = new DBConnection();
+			try {
+				DBQuery query = new DBQuery(dbcon.getConnection());
+				ResultSet rs = query.sendQuery("SELECT * FROM film");
+				while(rs.next())
+				{
+					System.out.println(rs.getString("titel"));
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 
 	}
 
