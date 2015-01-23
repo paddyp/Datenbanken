672a3513d5fb6252bffa2e397fc5e109787e2700
paddyp <patrikpezelj@Patriks-MacBook-Pro-2.local>
2015-01-24 00:36:05 +0100

    neuer Kunde Button
    mit dem neuer Kunde Button kommt man auf einen neuen Frame mit dem eine
    Maske geöffnet wird in der Textfelder und Beschriftung für die
    einzelnen Felder. Das ist noch nicht fertig kann aber gerne jederzeit
    erweitert werden. die Buttons haben nur einen Placebo Effekt und müssen
    noch impliziert werden.

--- Projekt/src/gui/GUI.java
+++ Projekt/src/gui/GUI.java
@@ -1,7 +1,14 @@
 package gui;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTable;
@@ -11,6 +18,10 @@ public class GUI extends JFrame {
 	
 	private JPanel panel;
 	private JTable table;
+	
+	private JButton newClient;
+	
+	
 		public GUI()
 		{
 			setSize(new Dimension(1000,1000));
@@ -19,15 +30,37 @@ public class GUI extends JFrame {
 			
 			String [][] data = {{"A"},{"B"}};
 			String [] title = {"titel"};
+			
+			
 			panel = new JPanel();
+			panel.setLayout(new GridBagLayout());
+			panel.setBackground(new Color(255,255,255));
+			GridBagConstraints c = new GridBagConstraints();
+			
+			newClient = new JButton();
+			newClient.setText("neuer Kunde");
+			newClient.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+						ClientFrame clientFrame = new ClientFrame();
+					
+				
+				}
+				
+			});
+			
 			table = new JTable(data,title);
 			
 			table.getModel().setValueAt("Cfasdkljfjadklsfjadsksdfjkdsfaklösdfajklasdlöfdsajklfadsjklfasdjkfasdkfsaadfkls", 0, 0);
 			panel.add(table.getTableHeader());
 			panel.add(table);
+			panel.add(newClient);
 			add(panel);
 		
 			
 			setVisible(true);
 		}
+		
 }
