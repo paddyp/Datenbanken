bdcbd401067d1e16b9f9be30741ab29dd224c1de
nadineGoeb <n.goebertshan@web.de>
2015-02-02 15:33:44 +0100

    menuBar fertig und an/abmelden
    menu bar ist fertig und man kann sich jetzt auch wieder abmelden

--- Projekt/src/gui/GUI.java
+++ Projekt/src/gui/GUI.java
@@ -18,190 +18,221 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
-import javax.swing.JTable;
 import javax.swing.JTextField;
 
 import business.DBQuery;
 
-
 public class GUI extends JFrame {
-	
+
+	private static final long serialVersionUID = 1L;
+
 	private JPanel panel;
-	
+
 	private boolean angemeldet = false;
 	private boolean admin = false;
-	
+
 	private JMenuBar menuBar;
+	
 	private JMenu datei;
 	private JMenu hinzufuegen;
-	private JMenuItem einstellungen;
-	
-	private JTextField benutzername;
-	private JPasswordField kennwort;
-	
-	private JLabel benutzerLabel;
-	private JLabel kennLabel;
-	
-	private JButton anmelden;
-	
-	private JButton newClient;
-	private JButton newMovie;
-	private JButton newKategorie;
-	private JButton newRoom;
-	private JButton newPlace;
-	private JButton newPerformance;
-	
-		public GUI()
-		{
-			setSize(new Dimension(1000,1000));
-			setTitle("DB Projekt");
-			setDefaultCloseOperation(EXIT_ON_CLOSE);
-			
-			final MovieFrame movieFrame = new MovieFrame();
-			final ClientFrame clientFrame = new ClientFrame();
-			final KategorieFrame kategorieFrame = new KategorieFrame();
-			final PlaceFrame placeFrame = new PlaceFrame();
-			final PerformanceFrame performanceFrame = new PerformanceFrame();
-			
-			
-			// Menu Bar
-			menuBar = new JMenuBar();
-			datei = new JMenu("Datei");
-			hinzufuegen = new JMenu("Hinzufügen");
-			
-			benutzerLabel = new JLabel("Benutzername :");
-			benutzername = new JTextField(10);
-			kennLabel = new JLabel("Kennwort :");
-			kennwort = new JPasswordField(10);
-			einstellungen = new JMenuItem("Einstellungen");
-			anmelden = new JButton("anmelden");
-			
-			anmelden.addActionListener(new ActionListener() {
-				
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					angemeldet = true;
-					anmelden.setVisible(!angemeldet);
-					System.out.println(angemeldet);
-					
-					getContentPane().repaint();
-					
-				}
-			});
-			
-			datei.add(einstellungen);
-			menuBar.add(datei);
-			menuBar.add(hinzufuegen);
-			
 
-			menuBar.add(benutzerLabel);
-			menuBar.add(benutzername);
-			menuBar.add(kennLabel);
-			menuBar.add(kennwort);
-			menuBar.add(anmelden);
-			
-			//
-			
-			
-			panel = new JPanel();
-			panel.setLayout(new GridBagLayout());
-			panel.setBackground(new Color(255,255,255));
-			GridBagConstraints c = new GridBagConstraints();
-			
-			newClient = new JButton();
-			newClient.setText("neuer Kunde");
-			newClient.addActionListener(new ActionListener() {
-				
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					// TODO Auto-generated method stub
-						clientFrame.setVisible(true);
-					
-				
-				}
-				
-			});
-			newMovie = new JButton("new Movie");
-			newMovie.addActionListener(new ActionListener() {
-				
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					// TODO Auto-generated method stub
-					movieFrame.setVisible(true);
-					
-				}
-			});
-			newKategorie = new JButton("neue Kategorie");
-			newKategorie.addActionListener(new ActionListener() {
-				
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					// TODO Auto-generated method stub
-					kategorieFrame.setVisible(true);
-					
-				}
-			});
-			
-			newRoom = new JButton("neuer Saal");
-			newRoom.addActionListener(new ActionListener() {
-				
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					// TODO Auto-generated method stub
-						String name =  JOptionPane.showInputDialog("Geben sie einen Bezeichnung für den neuen Saal ein");
-						if(name != null && name != "")
-						{
-							try {
-								DBQuery.sendInsertIntoQuery("saal", name);
-							} catch (SQLException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-						}
-					
-				}
-			});
-			
-			panel.add(newRoom);
-			
-			newPlace = new JButton("Platz");
-			newPlace.addActionListener(new ActionListener() {
-				
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					// TODO Auto-generated method stub
-						placeFrame.setVisible(true);
-					
-				}
-			});
-			panel.add(newPlace);
-			
-			newPerformance = new JButton("neue Vorstellung");
-			newPerformance.addActionListener(new ActionListener() {
-				
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					// TODO Auto-generated method stub
-					performanceFrame.setVisible(true);
-					
-					
-				}
-			});
-			
-			panel.add(newPerformance);
+	private JMenuItem einstellungen;
+	private JMenuItem neuerKunde;
+	private JMenuItem neuerFilm;
+	private JMenuItem neueKategorie;
+	private JMenuItem neuerSaal;
+	private JMenuItem neuerPlatz;
+	private JMenuItem neueVorstellung;
+
+	private JTextField benutzername;
+	private JPasswordField passwort;
+
+	private JLabel benutzerLabel;
+	private JLabel passwortLabel;
+
+	private JButton anmelden;
+	private JButton abmelden;
+	
+	public GUI() {
+		setSize(new Dimension(1000, 1000));
+		setTitle("DB Projekt");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		// Erzeugen der einzelnen Frames
+		final MovieFrame movieFrame = new MovieFrame();
+		final ClientFrame clientFrame = new ClientFrame();
+		final KategorieFrame kategorieFrame = new KategorieFrame();
+		final PlaceFrame placeFrame = new PlaceFrame();
+		final PerformanceFrame performanceFrame = new PerformanceFrame();
+
+		// Menu Bar Elemente erzeugen
+		menuBar = new JMenuBar();
 		
-			
-			
-			add(menuBar, BorderLayout.NORTH);
-			panel.add(newClient);
-			panel.add(newMovie);
-			panel.add(newKategorie);
-			
-			add(panel);
-			
+		datei = new JMenu("Datei");
+		hinzufuegen = new JMenu("Hinzufuegen");
+
+		benutzerLabel = new JLabel("Benutzername :");
+		benutzername = new JTextField(10);
+		passwortLabel = new JLabel("Kennwort :");
+		passwort = new JPasswordField(10);
+		einstellungen = new JMenuItem("Einstellungen");
+		anmelden = new JButton("Anmelden");
+		abmelden = new JButton("Abmelden");
+
+		neuerKunde = new JMenuItem("Neuer Kunde");
+		neuerFilm = new JMenuItem("Neuer Film");
+		neueKategorie = new JMenuItem("Neue Kategorie");
+		neuerSaal = new JMenuItem("Neuer Saal");
+		neuerPlatz = new JMenuItem("Neuer Platz");
+		neueVorstellung = new JMenuItem("Neue Vorstellung");
+
+		abmelden.setVisible(false);
 		
-			
-			setVisible(true);
-		}
+		anmelden.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				angemeldet = true;
+				anmelden.setVisible(false);
+				abmelden.setVisible(true);
+
+				benutzerLabel.setVisible(false);
+				benutzername.setVisible(false);
+				passwortLabel.setVisible(false);
+				passwort.setVisible(false);
+				
+				getContentPane().repaint();
+
+			}
+		});
 		
+		abmelden.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				angemeldet = false;
+				abmelden.setVisible(false);
+				anmelden.setVisible(true);
+
+				benutzerLabel.setVisible(true);
+				benutzername.setVisible(true);
+				passwortLabel.setVisible(true);
+				passwort.setVisible(true);
+				
+				getContentPane().repaint();
+
+			}
+		});
+
+		
+		// IN RUHE LASSEN PATRIKS
+		panel = new JPanel();
+		panel.setLayout(new GridBagLayout());
+		panel.setBackground(new Color(255, 255, 255));
+		GridBagConstraints c = new GridBagConstraints();
+
+		neuerKunde = new JMenuItem("Kunde");
+		neuerKunde.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				clientFrame.setVisible(true);
+			}
+
+		});
+
+		neuerFilm = new JMenuItem("Film");
+		neuerFilm.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				movieFrame.setVisible(true);
+
+			}
+		});
+
+		neueKategorie = new JMenuItem("Kategorie");
+		neueKategorie.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				kategorieFrame.setVisible(true);
+
+			}
+		});
+
+		neuerSaal = new JMenuItem("Saal");
+		neuerSaal.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				String name = JOptionPane
+						.showInputDialog("Geben sie einen Bezeichnung fuer den neuen Saal ein: ");
+				if (name != null && name != "") {
+					try {
+						DBQuery.sendInsertIntoQuery("saal", name);
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+
+			}
+		});
+
+		neuerPlatz = new JMenuItem("Platz");
+		neuerPlatz.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				placeFrame.setVisible(true);
+
+			}
+		});
+
+		neueVorstellung = new JMenuItem("Vorstellung");
+		neueVorstellung.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				performanceFrame.setVisible(true);
+
+			}
+		});
+
+
+		// Hinzufuegen
+		hinzufuegen.add(neuerKunde);
+		hinzufuegen.add(neuerFilm);
+		hinzufuegen.add(neueKategorie);
+		hinzufuegen.add(neuerSaal);
+		hinzufuegen.add(neuerPlatz);
+		hinzufuegen.add(neueVorstellung);
+		
+		// Einstellungen
+		datei.add(einstellungen);
+		menuBar.add(datei);
+		menuBar.add(hinzufuegen);
+
+		// Anmeldezeile
+		menuBar.add(benutzerLabel);
+		menuBar.add(benutzername);
+		menuBar.add(passwortLabel);
+		menuBar.add(passwort);
+		menuBar.add(anmelden);
+		menuBar.add(abmelden);
+
+		add(menuBar, BorderLayout.NORTH);
+
+		add(panel);
+
+		setVisible(true);
+	}
+
 }
