775437f28798fd717705f6d2470d521112c07688
visual_j <benedikt-ringlein@hotmail.de>
2015-02-05 15:58:34 +0100

    6rz

---
+++ Projekt/src/Objekte/VorstellungObjekt.java
@@ -0,0 +1,42 @@
+package Objekte;
+
+public class VorstellungObjekt {
+
+	// id zeit saal titel
+	private String id;
+	private String zeit;
+	private String saal;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getZeit() {
+		return zeit;
+	}
+	public void setZeit(String zeit) {
+		this.zeit = zeit;
+	}
+	public String getSaal() {
+		return saal;
+	}
+	public void setSaal(String saal) {
+		this.saal = saal;
+	}
+	public String getTitel() {
+		return titel;
+	}
+	public void setTitel(String titel) {
+		this.titel = titel;
+	}
+	private String titel;
+	public VorstellungObjekt(String id, String zeit, String saal, String titel) {
+		super();
+		this.id = id;
+		this.zeit = zeit;
+		this.saal = saal;
+		this.titel = titel;
+	}
+	
+}
