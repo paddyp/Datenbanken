551acd1f30393c15fa69a4d3d923549e327fa354
visual_j <benedikt-ringlein@hotmail.de>
2015-02-05 10:48:04 +0100

    added fillPlaceholders method. Still not pretected against sql incejtion, tho.

--- Projekt/src/business/DBQuery.java
+++ Projekt/src/business/DBQuery.java
@@ -1,6 +1,7 @@
 package business;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -8,22 +9,22 @@ import java.sql.Statement;
 public class DBQuery {
 
 	private static Statement stmt;
+	private static Connection con;
 
 	// connection errors never throws because connection is tested before in
 	// DBConnection
 
 	public DBQuery(Connection con) throws SQLException {
+		this.con = con;
 		this.stmt = con.createStatement();
-
 	}
 	
 	/**
+	 * Send String Query and get the result back
 	 * 
 	 * @param query
 	 * @return
 	 * @throws SQLException
-	 * 
-	 * Send String Query and get the result back
 	 */
 
 	public static ResultSet sendQuery(String query) throws SQLException {
@@ -57,17 +58,40 @@ public class DBQuery {
 		
 	}
 	
-	public static void toString(ResultSet rs,String... collums) throws SQLException{
+	/**
+	 * Takes a resultset and formats a string with the given columns on the console
+	 * @param rs The resultset to format
+	 * @param collums The collums to use
+	 * @throws SQLException when a database error occures
+	 */
+	public static String toString(ResultSet rs,String... collums) throws SQLException{
 	
-			while(rs.next())
-			{
-				for(String collum:collums)
-				{
-					System.out.print(rs.getString(collum) + " ");
-				}
-				System.out.println();
-			}
-
+		StringBuilder sb = new StringBuilder();
 		
+		while(rs.next())
+		{
+			sb.append(rs.getString(collums[0]));
+			for(int i=1;i<collums.length;i++)
+			{
+				sb.append(" | " + rs.getString(collums[i]));
+			}
+		}
+		
+		return sb.toString();
 	}
+	
+	/**
+	 * This fills placeholders in a query with the actual data
+	 * @param query The query with placeholders
+	 * @param replacements The replacements. The first one replaces %1%, the second one %2% and so on
+	 * @return Returns the query with replacements
+	 * @throws SQLException 
+	 */
+	public String fillPlaceholders(String query, String... replacements) throws SQLException{
+		for(int i = 1; i<replacements.length; i++){
+			query.replaceAll("%"+i+"%", replacements[i]);
+		}
+		return query;
+	}
+	
 }
